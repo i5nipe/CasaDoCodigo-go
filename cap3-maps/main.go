7b7701bc353fcd0e41930067efdc8a55f1eac050
isnipe <thalles.isnipe@gmail.com>
2021-04-14 09:38:10 -0300

    Adicionado := no for

--- cap3-maps/main.go
+++ cap3-maps/main.go
@@ -8,12 +8,13 @@ import (
 
 // Conta a frequência de palavras com as mesmas iniciais.
 func main() {
-	palavras := os.Args[1:]	
+	palavras := os.Args[1:]
+
+	fmt.Println(colherEstatisticas(palavras))
 }
 
-
 // map, array associativo ou dicionário tem funcionamento
-// parecido com os dicionários de python 
+// parecido com os dicionários de python
 // é uma coleção de pares chave/valor em que cada chave é unica
 // e armazena um único valor.
 // Nessa função a chave é a letra inicial da palavra e o valor
@@ -21,17 +22,23 @@ func main() {
 func colherEstatisticas(palavras []string) map[string]int {
 	estatisticas := make(map[string]int)
 
-	for _,palavra range palavras {
+	for _, palavra := range palavras {
 
 		// Converte a inicial da palavra para maiuscula e armazena na variavel
 		inicial := strings.ToUpper(palavra[0])
 
 		// Estatisticas chama o map da inicial e retorna dois dados
 		// o primeiro é o valor daquela chave, no nosso caso um int
-		// o segundo é um bool que volta false se não encontrar a chave 
+		// o segundo é um bool que volta false se não encontrar a chave
 		// e true se encontrar
 		contador, encontrado := estatisticas[inicial]
 
-	}
+		if encontrado {
+			estatisticas[inicial] = contador + 1
+		} else {
+			estatisticas[inicial] = 1
+		}
 
+	}
+	return estatisticas
 }

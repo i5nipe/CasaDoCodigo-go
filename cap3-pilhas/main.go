f5e1e8d6a3f7e31eb2d64964371252e82f519639
isnipe <thalles.isnipe@gmail.com>
2021-04-15 08:57:21 -0300

    Inicio do sonho

--- cap3-pilhas/main.go
+++ cap3-pilhas/main.go
@@ -5,5 +5,22 @@ import (
 )
 
 func main() {
-	fmt.Println("vim-go")
+	pilha := Pilha{}
+
+	// Repare que todos os metodos chamados se iniciam com letra maiúscula
+	// até mesmo o println do fmt. O que acontece é que todo identificador
+	// que se inicia com letra maiúscula é automaticamente exportado e visível
+	// fora do pacote.
+	fmt.Println("Pilha criada com tamanho de ", pilha.Tamanho()) // Retorna 0
+	fmt.Println("Vazia?", pilha.Vazia())                         // Retorna true
+
+	// Empilhando alguns objetos
+	pilha.Empilhar("Go")
+	pilha.Empilhar("2009")
+	pilha.Empilhar("3.14")
+	pilha.Empilhar("Fim")
+
+	fmt.Println("Tamanho após empilhar 4 valores: ", pilha.Tamanho()) // Retorna 4
+	fmt.Println("Vazia?", pilha.Vazia())                              // Retorna false
+
 }

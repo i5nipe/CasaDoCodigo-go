0571325bb42efc09f76f11063bdc75cef622ec79
isnipe <thalles.isnipe@gmail.com>
2021-04-16 11:01:35 -0300

    Comentarios

--- Cap4/loop-infinito/loop_infinito.go
+++ Cap4/loop-infinito/loop_infinito.go
@@ -7,16 +7,23 @@ import (
 )
 
 func main() {
+	//Precisamos de uma seed para geração de números "aleatorios"
+	// Nesse casa estamos usando o timestamp do momento da execução "time.Now()"
+	// E o número de nanosegundos desde 1° de janeiro de 1970 "UnixNano()"
 	rand.Seed(time.Now().UnixNano())
 	n := 0
 
+	//Inicia um loop infinito
 	for {
 		n++
 
+		// A função Intn gera um número pseudo aleatório entre 0 até 4200
 		i := rand.Intn(4200)
 		fmt.Println(i)
 
+		// Se o resto da divição de i por 42 for igual a zero
 		if i%42 == 0 {
+			// Por padrão o Break sai do loop mais próximo
 			break
 		}
 	}

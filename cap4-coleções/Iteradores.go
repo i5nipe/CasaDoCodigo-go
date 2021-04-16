224bce856433dc9ea1c94b41a31ff5bcd9e89fa6
isnipe <thalles.isnipe@gmail.com>
2021-04-16 09:57:54 -0300

    N/A Iteradores

--- cap4-coleções/Iteradores.go
+++ cap4-coleções/Iteradores.go
@@ -13,15 +13,31 @@ func main() {
 	}
 	fmt.Println(a)
 
-	//
+	// -----------------
+
+	// Podemos utilizar a syntax do C para fazer uma estrutura de repetição
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
 
-	//
+	// -----------------
+
+	// E também é possivel usar a váriavel fora do looping se declarada antes
 	var i2 int
-	for i2 := 0; i2 < 10; i2++ {
+	for i2 = 0; i2 < 10; i2++ {
 		// ...
 	}
 	fmt.Println(i)
+
+	// -----------------
+
+	// Podendo omitir qualquer parte da syntax e deixado o ";"
+	// Porém se for utilizada apenas uma condição como no primeiro exemplo o ";" pode ser omitido
+	var i3 int
+	for i3 = 0; i3 < 10; {
+		k += 1
+	}
+
+	// -----------------
+	numeros := []int{1, 2, 3, 4, 5, 6, 7}
 }

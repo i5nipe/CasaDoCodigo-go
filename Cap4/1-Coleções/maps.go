162e7f538154f9f733baf8499e00f17dd65d84f8
isnipe <thalles.isnipe@gmail.com>
2021-04-19 10:52:05 -0300

    Adicionado nome correto as variaveis

--- Cap4/1-Coleções/maps.go
+++ Cap4/1-Coleções/maps.go
@@ -66,8 +66,8 @@ func main() {
 
 	// Como cada chave tem um valor único podemos apenas adicionar um valor
 	// normalmente e ele será sobrescrito.
-	idade["João"] = 25
-	fmt.Println(idade["João"]) // 25
+	idades["João"] = 25
+	fmt.Println(idades["João"]) // 25
 
 	// ----------------------{ Removendo valores }-------------------------
 	// func delete(m map[TipoChave]TipoValor, chave TipoChave)

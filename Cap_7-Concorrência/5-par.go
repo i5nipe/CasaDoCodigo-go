f641fea86b855ba7c28e2e46c684cab030c2c630
isnipe <thalles.isnipe@gmail.com>
2021-04-20 14:13:38 -0300

    Pares funcionando

--- Cap_7-Concorrência/5-par.go
+++ Cap_7-Concorrência/5-par.go
@@ -11,7 +11,7 @@ func separar(nums []int, i, p chan<- int, pronto chan<- bool) {
 	pronto <- true
 }
 func main() {
-	i, p, pronto := make(chan int), make(chan int)
+	i, p := make(chan int), make(chan int)
 	pronto := make(chan bool)
 	nums := []int{1, 2, 3, 42, 2, 324, 234, 2354, 2, 2, 112, 1, 123, 34, 345, 3, 2, 5, 346, 346, 3, 32222}
 	go separar(nums, i, p, pronto)
@@ -22,10 +22,10 @@ func main() {
 	for !fim {
 		select {
 		case n := <-i:
-			impares = append(impares,n)
+			impares = append(impares, n)
 		case n := <-p:
 			pares = append(pares, n)
 		case fim = <-pronto:
 		}
 	}
-}:
+}

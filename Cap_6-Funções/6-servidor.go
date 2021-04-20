9d4033cdcecb36bd4c517a19b02db903292326b1
isnipe <thalles.isnipe@gmail.com>
2021-04-20 09:31:45 -0300

    Comentario no servidor simples

--- Cap_6-Funções/6-servidor.go
+++ Cap_6-Funções/6-servidor.go
@@ -8,8 +8,14 @@ import (
 
 func main() {
 	http.HandleFunc(
+		// Especificamos que o http.HandleFunc atenderá as requisições
+		// da URL /tempo .
 		"/tempo",
 		func(w http.ResponseWriter, r *http.Request) {
+			// Utilizamos a função Fprintf pela primeira vez,
+			// Ela funciona  semenhante a Printf com um porém
+			// Seu primeiro argumento deve receber um io.Writer
+			// onde sua saida sera escrita.
 			fmt.Fprintf(w, "%s",
 				time.Now().Format("2006-01-02 15:04:05"))
 		})

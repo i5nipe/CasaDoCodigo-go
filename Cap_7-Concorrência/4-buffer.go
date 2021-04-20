b9274668d619f73c9abb2f3de353930aca2ce0c5
isnipe <thalles.isnipe@gmail.com>
2021-04-20 11:44:59 -0300

    Buffer

---
+++ Cap_7-Concorrência/4-buffer.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Criamos um channel com buffer de 3
+	c := make(chan int, 3)
+	go produzir(c)
+
+	// Definimos um buffer de 3 e estamos enviando 4 valores
+	// iriamos recebe no terminal um erro deadlock se não
+	// fosse o close no final da função produzir()
+	fmt.Println(<-c, <-c, <-c, <-c)
+}
+
+func produzir(c chan int) {
+	c <- 1
+	c <- 2
+	c <- 3
+
+	close(c)
+}

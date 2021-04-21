3b32666f6293a1b525eab74c3b17a0fcc72cbbdf
isnipe <thalles.isnipe@gmail.com>
2021-04-21 08:17:57 -0300

    Adicionado comentarios em type_conversion

--- Cap5/2-type_conversion.go
+++ Cap5/2-type_conversion.go
@@ -4,10 +4,12 @@ import "fmt"
 
 type ListaDeCompras []string
 
+// Uma função que precisa de uma string para funcionar
 func imprimirSlice(slice []string) {
 	fmt.Println("Slice: ", slice)
 }
 
+// Uma função que precisa de uma ListaDeCompras para funcionar
 func imprimirLista(lista ListaDeCompras) {
 	fmt.Println("Lista: ", lista)
 }
@@ -16,6 +18,10 @@ func main() {
 	lista := ListaDeCompras{"Alface", "Atum", "Leite"}
 	slice := []string{"Alface", "Atum", "Leite"}
 
+	// Se reparar bem nós estamos enviando a variavel errada
+	// para as função, de acordo com seu tipo, Porem
+	// o type conversion aqui está funcionando, Pois são
+	// tipos compativeis.
 	imprimirSlice([]string(lista))
 	imprimirLista(ListaDeCompras(slice))
 }

e978c501d6e187914bc9041c68d314f61a3b222b
isnipe <thalles.isnipe@gmail.com>
2021-04-21 08:09:34 -0300

    Inicio type conversion

---
+++ Cap5/2-type_conversion.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type ListaDeCompras []string
+
+func imprimirSlice(slice []string) {
+	fmt.Println("Slice: ", slice)
+}
+
+func imprimirLista(lista ListaDeCompras) {
+	fmt.Println("Lista: ", lista)
+}
+
+func main() {
+
+}

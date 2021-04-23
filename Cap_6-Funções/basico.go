00d70d2aacc70c20ae3ab0384ff1506b49bdfc58
isnipe <thalles.isnipe@gmail.com>
2021-04-23 09:28:24 -0300

    Inicio de funções

---
+++ Cap_6-Funções/basico.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// Uma função basica que não receve nenhum argumento
+// e não retorna nada.
+func ImprimirVersao() {
+	fmt.Println("0.666")
+}
+func main() {
+	ImprimirVersao()
+}
